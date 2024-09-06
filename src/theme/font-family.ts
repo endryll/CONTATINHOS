7a219e95eb24f107e19c41bd77056dc134b6774a
endryll <endryll.silverio.dantas@gmail.com>
2024-09-06 09:14:05 -0300

    [feat] :sparkles: :Implementação da barra de pesquisa pelo nome

---
+++ src/theme/font-family.ts
@@ -0,0 +1,5 @@
+export const fontFamily = {
+    regular: "Ubuntu_400Regular",
+    bold: "Ubuntu_7000Regular",
+    medium: "Ubunto_500Medium",
+}
--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -1,6 +1,8 @@
 import { colors } from './colors';
+import { fontFamily } from './font-family';
 
 export const theme = {
     colors,
+    fontFamily,
 }
 
